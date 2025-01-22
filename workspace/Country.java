728acc31945f53976bc3988045d90ca4dcaa6dc8
LAZARBUM <133454046+LAZARBUM@users.noreply.github.com>
2025-01-22 04:11:16 +0000

    January 20th, 2025

--- workspace/Country.java
+++ workspace/Country.java
@@ -1,14 +1,30 @@
 public class Country
 {
-  // add private instance variables for the name, capital, language, and image file.
+ // add private instance variables for the name, capital, language, and image file.
+ private String name;
+ private String capital;
+ private String primaryLanguage;
+ private String imagefile;
+ // add constructors
+ public Country() {}
 
-  // add constructors
 
-  // Write accessor/get methods for each instance variable that returns it.
+ // Constructor with 4 arguments
+ public Country(String name, String capital, String primaryLanguage, String imagefile) {
+     this.name = name;
+     this.capital = capital;
+     this.primaryLanguage = primaryLanguage;
+     this.imagefile = imagefile;
+ }
+ // Write accessor/get methods for each instance variable that returns it.
+ public String getName() { return name; }
+ public String getCapital() { return capital; }
+ public String getPrimaryLanguage() { return primaryLanguage; }
+ public String getImagefile() { return imagefile; }
 
-  // Write a toString() method that returns a concatenated String of 3 of the instance variables in a sentence like "..'s capital is .. and its primary language is ..."
- 
 
-
-  
+ // toString method
+ public String toString() {
+     return name + "'s capital is " + capital + " and its primary language is " + primaryLanguage + ".";
+ }
 }
--- workspace/Main.java
+++ workspace/Main.java
@@ -1,14 +1,16 @@
+// Steve Thomas Period 3
+// January 17th, 2025
+// GeoGame - Countries, Quiz, and Review varous countries around the Earth.
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.*;
 import java.io.*;
 
-public class Main 
-{
-
+public class Main {
   // array of 10 Country objects
-  private Country[] countryArray = new Country[10];  
+  private Country[] countryArray = new Country[10]; 
   // index of current shown country
   private int index = 0;
 
@@ -17,7 +19,7 @@ public class Main
   private ImageIcon img;
   private JLabel imageLabel;
   private JLabel outputLabel;
-  
+
   public static void main(String[] args) {
     // Create the GUI
     Main gui = new Main();
@@ -25,111 +27,133 @@ public class Main
     gui.showCountry();
   }
 
-  /* loadCountries() reads in the data from the countries-data.csv file and fills in the countryArray with data. You need to add the loop that reads in the country data into the array. */
-  public void loadCountries() 
-  {
+  /*
+   * Pre-condition: CSV file 'countries-data.csv' exists in the specified path.
+   * Post-condition: countryArray is filled with Country objects created from the CSV data.
+   */
+  public void loadCountries() {
     // Open the data file - do not change
     File file = new File("/workspaces/Countries/workspace/countries-data.csv");
     Scanner scan = null;
     try {
       scan = new Scanner(file);
-    } catch(FileNotFoundException e) { 
-        System.out.println("File not found");     
+    } catch(FileNotFoundException e) {
+      System.out.println("File not found");
     }
     
     // Write a for loop that goes through the countryArray.
-    // for(int i ....) {
-    // Do the following inside the loop
+    for (int i = 0; i < countryArray.length; i++) {
+      // Do the following inside the loop
       String input = scan.nextLine();
       String[] data = input.split(",");
       System.out.println("Read in " + data[0]);
       // inside the loop, create a new Country using your constructor with 4 arguments and pass in data[0], data[1], data[2], data[3] as arguments.
-     // inside the loop, set countryArray[i] to the created Country object
-     
-    
+      Country country = new Country(data[0], data[1], data[2], data[3]);
+      // inside the loop, set countryArray[i] to the created Country object
+      countryArray[i] = country;
+    }
   }
 
-  /* showCountry() will show the image associated with the current country. It should get the country at index from the countryArray. It should use its get method to get its image file name and use the code below to put the image in the GUI.
-  */
+  /*
+   * Pre-condition: The index must be within the bounds of countryArray.
+   * Post-condition: The imageLabel displays the image of the country at the current index.
+   */
   public void showCountry() {
     // Get the country at index from countryArray
-    
+    Country c = countryArray[index];
     // Use its get method to get the its image file name and save it into imagefile variable below instead of worldmap.jpg.
-    String imagefile = "worldmap.jpg";
+    String imagefile = c.getImagefile();
     // Use the following code to create an new Image Icon and put it into the GUI
-    img = new ImageIcon("/workspaces/Countries/workspace/"+imagefile);
+    img = new ImageIcon("/workspaces/Countries/workspace/" + imagefile);
     imageLabel.setIcon(img);
   }
-  
-  /* nextButton should increment index. If the index is greater than 9, reset it back to 0. Clear the outputLabel to empty string using setText, and call showCountry();*/
-  public void nextButtonClick()
-  {
-    
-  }
-  
-  /* reviewButton should get the country at index from the countryArray, call its toString() method and save the result, print it out with System.out.println and as an argument to outputLabel.setText( text to print out ); */
-  public void reviewButtonClick()
-  {
-     
-  }
 
-  /* quizButton should clear the outputLabel (outputLabel.setText to empty string), get the country at index from countryArray, print out a question about it like What country is this? and/or What's this country's capital?. Get the user's answer using scan.nextLine() and check if it is equal to the country's data using its get methods and print out correct or incorrect.
-  */
-  public void quizButtonClick()
-  {
-    Scanner scan = new Scanner(System.in); 
-    
-    
-    
+  /*
+   * Pre-condition: Button click event triggers this method.
+   * Post-condition: Index is incremented, and the next country's image is displayed. 
+   * If index goes out of bounds, it is reset to 0.
+   */
+  public void nextButtonClick() {
+    index++;
+    if (index >= countryArray.length) {
+      index = 0;
+    }
+    outputLabel.setText("");
+    showCountry();
   }
 
+  /*
+   * Pre-condition: Button click event triggers this method.
+   * Post-condition: Displays the current country's information in the output label.
+   */
+  public void reviewButtonClick() {
+    Country c = countryArray[index];
+    String info = c.toString();
+    System.out.println(info);
+    outputLabel.setText(info);
+  }
 
-
+  /*
+   * Pre-condition: Button click event triggers this method.
+   * Post-condition: Prompts the user with a quiz question and shows whether the answer is correct or not.
+   */
+  public void quizButtonClick() {
+    outputLabel.setText(""); // Clear the outputLabel
+    Country c = countryArray[index];
+    String question = "What country is this?";
+    System.out.println(question);
+    // Display the question in the output label
+    outputLabel.setText(question);
+    // Create a dialog box to get user's answer
+    String answer = JOptionPane.showInputDialog(jFrame, question);
+    
+    if (answer != null && answer.equalsIgnoreCase(c.getName())) {
+      System.out.println("Correct!");
+      JOptionPane.showMessageDialog(jFrame, "Correct!", "Result", JOptionPane.INFORMATION_MESSAGE);
+    } else {
+      System.out.println("Incorrect. The correct answer is " + c.getName());
+      JOptionPane.showMessageDialog(jFrame, "Incorrect. The correct answer is " + c.getName(), "Result", JOptionPane.ERROR_MESSAGE);
+    }
+  }
 
   /* Do NOT change anything below here */
   /* The Main() constructor is finished and will construct the GUI */
-public Main() {
+  public Main() {
     jFrame.setLayout(new FlowLayout());
     jFrame.setSize(500, 360);
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // buttons at the top
-        JButton reviewButton = new JButton("Review");
-        JButton quizButton = new JButton("Quiz");
-        JButton newButton = new JButton("Next");
-        jFrame.add(reviewButton);
-        jFrame.add(quizButton);
-        jFrame.add(newButton);
-        
-        // create a new image icon
-        img = new ImageIcon("worldmap.jpg");
-        // create a label to display image
-        imageLabel = new JLabel(img);
-        // and one for output
-        outputLabel = new JLabel();
-        jFrame.add(imageLabel);
-        jFrame.add(outputLabel);
-        jFrame.setVisible(true);
-        // add event listener for button click
-        reviewButton.addActionListener(new ActionListener() {
-    public void actionPerformed(ActionEvent e) 
-    {
-      reviewButtonClick();
-    }
-        });
-    quizButton.addActionListener(new ActionListener() {
-    public void actionPerformed(ActionEvent e) 
-    {
-      quizButtonClick();
-    }
-    });
-   
-   newButton.addActionListener(new ActionListener()  {
-    public void actionPerformed(ActionEvent e) 
-    {
-      nextButtonClick();
-    }
-   });
-}
-  
+    jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    // buttons at the top
+    JButton reviewButton = new JButton("Review");
+    JButton quizButton = new JButton("Quiz");
+    JButton newButton = new JButton("Next");
+    jFrame.add(reviewButton);
+    jFrame.add(quizButton);
+    jFrame.add(newButton);
 
+    // create a new image icon
+    img = new ImageIcon("worldmap.jpg");
+    // create a label to display image
+    imageLabel = new JLabel(img);
+    // and one for output
+    outputLabel = new JLabel();
+    jFrame.add(imageLabel);
+    jFrame.add(outputLabel);
+    jFrame.setVisible(true);
+    // add event listener for button click
+    reviewButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        reviewButtonClick();
+      }
+    });
+    quizButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        quizButtonClick();
+      }
+    });
+    newButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        nextButtonClick();
+      }
+    });
+  }
 }
